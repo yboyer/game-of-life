1073ec8d3a62a98f31d0cd97aa09527a8cf8fa45
yboyer <bhyw180@gmail.com>
2016-12-30 18:45:32 +0100

    Update fish cycles definitions

--- src/model/Fish.java
+++ src/model/Fish.java
@@ -1,6 +1,10 @@
 package model;
 
 abstract class Fish extends Cell {
+    private static int DEATHCYCLE = 20;
+    private static int DEATHCYCLEAVG = DEATHCYCLE / 3;
+    private static int BREEDINGCYCLE = 4;
+    private static int BREEDINGCYCLEAVG = BREEDINGCYCLE / 3;
     private int age;
     private int breedingCycle;
     private int deathCycle;
@@ -9,7 +13,13 @@ abstract class Fish extends Cell {
         super(y, x);
 
         this.age = 0;
-        this.deathCycle = (int) Math.random() * 30;
-        this.breedingCycle = (int) (Math.random() * 30);
+
+        int minDeathCycle = DEATHCYCLE - DEATHCYCLEAVG;
+        int maxDeathCycle = DEATHCYCLE + DEATHCYCLEAVG;
+        this.deathCycle = (int) (Math.random() * (maxDeathCycle - minDeathCycle)) + minDeathCycle;
+
+        int minBreedingCycle = BREEDINGCYCLE - BREEDINGCYCLEAVG;
+        int maxBreedingCycle = BREEDINGCYCLE + BREEDINGCYCLEAVG;
+        this.breedingCycle = (int) (Math.random() * (maxBreedingCycle - minBreedingCycle)) + minBreedingCycle;
     }
 }
