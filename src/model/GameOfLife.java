792375da14212c7fe2d4bd9d3c4332f416f309da
yboyer <bhyw180@gmail.com>
2016-12-30 16:47:49 +0100

    Add a constructor to set a default sea as a grid
    Add an exception to the populate method

--- src/model/GameOfLife.java
+++ src/model/GameOfLife.java
@@ -6,4 +6,26 @@ public class GameOfLife {
         // Sea sea = new Sea(5, 5, 2, 10);
         System.out.println(sea);
     }
+
+    private static Sea getDefault() {
+        int height = 5;
+        int width = 10;
+
+        Cell[][] grid = new Cell[height][width];
+
+        grid[0][0] = new Pilchard(0, 0);
+        grid[0][1] = new Shark(0, 1);
+
+        int x, y;
+        for (int i = (height * width) - 1; i >= 0; i--) {
+            y = i % height;
+            x = i / height;
+
+            if (grid[y][x] == null) {
+                grid[y][x] = new Water(y, x);
+            }
+        }
+
+        return new Sea(grid);
+    }
 }
--- src/model/Sea.java
+++ src/model/Sea.java
@@ -1,63 +1,118 @@
 package model;
 
+import java.util.*;
+
+
 class Sea {
     private Cell[][] grid;
-    private int sizeX;
-    private int sizeY;
+    private int height;
+    private int width;
 
     /**
-     * Generate and poluplate a sea
-     * @param sizeX Width of the sea
-     * @param sizeY Height of the sea
-     * @param nbPilchards Count of pilchards to add
+     * Generate and populate the sea
+     * @param height Height of the sea
+     * @param width Width of the sea
      * @param nbSharks Count of sharks to add
+     * @param nbPilchards Count of pilchards to add
      */
-    public Sea(int sizeX, int sizeY, int nbPilchards, int nbSharks) {
-        this.grid = new Cell[sizeY][sizeX];
+    public Sea(int height, int width, int nbSharks, int nbPilchards) throws Exception {
+        this(height, width);
 
         populate(nbSharks, nbPilchards);
+    }
+
+    /**
+     * Generate the sea
+     * @param height Height of the sea
+     * @param width Width of the sea
+     */
+    public Sea(int height, int width) {
+        this.height = height;
+        this.width = width;
+
+        this.grid = new Cell[getHeight()][getWidth()];
+    }
+
+    /**
+     * Generate and populate the sea
+     * @param defaultCells A custom set of cell
+     */
+    public Sea(Cell[][] defaultCells) {
+        this(defaultCells.length, defaultCells[0].length);
+
+        this.grid = defaultCells;
     }
 
     /**
      * Consctructor with default values
      */
     public Sea() {
-        this(500, 500, 20, 100);
+        this(500, 500);
+        try {
+            populate(20, 10);
+        } catch (Exception e) {}
+    }
+
+    /**
+     * Get the height of the sea
+     * @return The height of the sea
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * Get the width of the sea
+     * @return The width of the sea
+     */
+    public int getHeight() {
+        return height;
     }
 
     /**
      * Populate the whole sea
-     * @param nbPilchards Number of pilchards
      * @param nbSharks Number of sharks
+     * @param nbPilchards Number of pilchards
      */
-    private void populate(int nbPilchards, int nbSharks) {
-        int i;
-        int x;
-        int y;
+    public void populate(int nbSharks, int nbPilchards) throws Exception {
+        int nbFish = nbSharks + nbPilchards;
+        int nbCell = this.width * this.height;
+        if (nbFish >= nbCell) {
+            throw new Exception("Too many fish (" + nbFish + ") to add on the sea (" + nbCell + ")");
+        }
+        int i, x, y;
 
         // Add pilchards
         for (i = 0; i < nbPilchards; i++) {
-            x = i % sizeX;
-            y = i / sizeY;
+            y = i % getHeight();
+            x = i / getHeight();
             grid[y][x] = new Pilchard(y, x);
         }
 
         // Add sharks
         for (i = nbPilchards; i < nbPilchards + nbSharks; i++) {
-            x = i % sizeX;
-            y = i / sizeY;
-            grid[i / sizeY][i % sizeX] = new Shark(y, x);
+            y = i % getHeight();
+            x = i / getHeight();
+            grid[y][x] = new Shark(y, x);
+        }
+
+        // Add water
+        for (i = nbPilchards + nbSharks; i < getHeight() * getWidth(); i++) {
+            y = i % getHeight();
+            x = i / getHeight();
+            grid[y][x] = new Water(y, x);
         }
 
         // Randomize the whole sea
-        for (int y = 0; y < sizeY; y++) {
-            for (int x = 0; x < sizeX; x++) {
+        for (y = 0; y < getHeight(); y++) {
+            for (x = 0; x < getWidth(); x++) {
                 // Get coordinates of a random cell
-                rX = Math.random() * this.sizeX();
-                rY = Math.random() * this.sizeX();
+                int rX = (int) (Math.random() * this.getWidth());
+                int rY = (int) (Math.random() * this.getHeight());
 
                 // Swap current cell with a random one
-                temp = grid[y][x];
+                Cell temp = grid[y][x];
+
                 grid[y][x] = grid[rY][rX].setX(x).setY(y);
                 grid[rY][rX] = temp.setX(rX).setY(rY);
             }
@@ -70,31 +125,52 @@ class Sea {
      * @return List of the nearby cells
      */
     private ArrayList<Cell> getNearbyCells(Cell cell) {
-        ArrayList<Cell> cells = ArrayList<Cell>();
+        ArrayList<Cell> cells = new ArrayList<Cell>();
         int minY = cell.getY() - 1;
         int minX = cell.getX() - 1;
         int maxY = cell.getY() + 1;
         int maxX = cell.getX() + 1;
 
-        // Reformat value for interation
+        // Set the values into the sea bounds
         if (minX < 0) {
             minX = 0;
         }
         if (minY < 0) {
             minY = 0;
         }
-        if (maxX > sizeX - 1) {
-            maxX = sizeX - 1;
+        if (maxX > getWidth() - 1) {
+            maxX = getWidth() - 1;
         }
-        if (maxY > sizeY - 1) {
-            maxY = sizeY - 1;
+        if (maxY > getHeight() - 1) {
+            maxY = getHeight() - 1;
         }
 
         // Loop to nearby cells
         for (int y = minY; y <= maxY; y++) {
             for (int x = minX; x <= maxX; x++) {
-                cells.push(grid[y][x]);
+                if (y != cell.getY() || x != cell.getX()) {
+                    cells.add(grid[y][x]);
+                }
             }
         }
+
+        return cells;
+    }
+
+    /**
+     * Return the grid as a string
+     * @return The grid
+     */
+    public String toString() {
+        String display = "\n";
+
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                display += grid[y][x].toString();
+            }
+            display += "\n";
+        }
+
+        return display;
     }
 }
