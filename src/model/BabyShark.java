341a93040cfd3812432186a3cf8a378d6b67efa0
yboyer <bhyw180@gmail.com>
2017-01-03 20:21:06 +0100

    Add baby shark state
    Add states to shark
    Add famine to sharks

---
+++ src/model/BabyShark.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Represents a state of shark. The baby shark.
+ *
+ * He moves to a random nearby cell. If this cell contains a pilchard, he eats
+ * him.
+ */
+class BabyShark implements FishState {
+    private static int AGE_STEP = 2;
+
+    @Override
+    public void move(Shark shark, Sea sea) {
+        ArrayList<Cell> cells = sea.getNearbyCells(shark);
+        cells = shark.getReachableCells(cells);
+
+        // Move to a random cell
+        // If this cell contains a pilchard, the baby shark eats him
+        if (!cells.isEmpty()) {
+            int index = (int) (Math.random() * cells.size());
+            Cell destCell = cells.get(index);
+
+            sea.transposeCells(shark, destCell);
+
+            if (destCell instanceof Pilchard) {
+                System.out.println("  *eaten by {" + shark.getY() + ", " + shark.getX() + "}*" + " as °");
+                sea.kill((Fish) destCell);
+                shark.resetLastEatCycle();
+            }
+        }
+    }
+
+    @Override
+    public void grow(FishStateContext fish) {
+        if (fish.getAge() == AGE_STEP) {
+            System.out.println("Update to teen");
+            // fish.setState(new TeenShark());
+        }
+    }
+}
--- src/model/Shark.java
+++ src/model/Shark.java
@@ -1,6 +1,7 @@
 package model;
 
-class Shark extends Fish {
+import java.util.ArrayList;
+
 /**
  * Represents a shark.
  *
@@ -10,12 +11,16 @@ class Shark extends Fish {
  * @see model.TeenShark
  * @see model.AdultShark
  */
+class Shark extends Fish implements FishStateContext {
     private static int DEATHCYCLE = 20;
     private static int DEATHCYCLE_RANGE = DEATHCYCLE / 3;
     private static int BREEDINGCYCLE = 4;
     private static int BREEDINGCYCLE_RANGE = BREEDINGCYCLE / 3;
+    private static int FAMINELIMIT = 4;
     protected int breedingCycle;
     protected int deathCycle;
+    private int lastEatCycle;
+    private FishState state;
 
     /**
      * The shark constructor
@@ -25,8 +30,33 @@ class Shark extends Fish {
     public Shark(int y, int x) {
         super(y, x);
 
+        this.state = new BabyShark();
+
+        this.lastEatCycle = 0;
+
         this.deathCycle = Fish.getCycleAverage(DEATHCYCLE, DEATHCYCLE_RANGE);
         this.breedingCycle = Fish.getCycleAverage(BREEDINGCYCLE, BREEDINGCYCLE_RANGE);
+    }
+
+    @Override
+    public void setState(FishState state) {
+        this.state = state;
+    }
+
+    @Override
+    public int getAge() {
+        return super.getAge();
+    }
+
+    @Override
+    protected void grow() {
+        super.grow();
+        state.grow(this);
+    }
+
+    @Override
+    protected void move(Sea sea) {
+        state.move(this, sea);
     }
 
     @Override
@@ -37,6 +67,45 @@ class Shark extends Fish {
     @Override
     protected int getBreedingCycle() {
         return this.breedingCycle;
+    }
+
+    @Override
+    protected boolean isFamineDeath() {
+        if (this.getLastEatCycle() == FAMINELIMIT) {
+            return true;
+        }
+        this.lastEatCycle++;
+        return false;
+    }
+
+    /**
+     * Reset the last time he eats
+     */
+    protected void resetLastEatCycle() {
+        this.lastEatCycle = 0;
+    }
+
+    /**
+     * Get the last time he eats
+     * @return The last eat cycle
+     */
+    protected int getLastEatCycle() {
+        return lastEatCycle;
+    }
+
+    /**
+     * Filter the input list to remove all unreachable cells
+     * @param cells The cells to filter
+     * @return The filtered list
+     */
+    protected ArrayList<Cell> getReachableCells(ArrayList<Cell> cells) {
+        // Remove sharks of the list
+        for (int c = cells.size() - 1; c >= 0; c--) {
+            if (cells.get(c) instanceof Shark) {
+                cells.remove(c);
+            }
+        }
+        return cells;
     }
 
     @Override
